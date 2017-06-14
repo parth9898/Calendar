62268db5a50d602239d835cf96107c8f33d8dd01
Parth Chauhan <chauhanparth159@gmail.com>
2017-06-14 20:23:46 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,18 +1,30 @@
-package abstractClasses;
+package Clone;
 
-public class Student extends Person
+public class Student implements Cloneable
 {
 
-	
-		 private String major;
-		 public Student(String n, String m)
-		 {
-		 super(n);
-		 major = m;
-		 }
-		
-		 public String getDescription()
-		 {
-		 return "a student majoring in " + major;
-		 }	
-}
+	int rollno;  
+	String name;  
+	  
+	Student(int rollno,String name){  
+	this.rollno=rollno;  
+	this.name=name;  
+	}  
+	  
+	public Object clone()throws CloneNotSupportedException{  
+	return super.clone();  
+	}  
+	  
+	public static void main(String args[]){  
+	try{  
+	Student s1=new Student(101,"parth");  
+	  
+	Student s2=(Student)s1.clone();  
+	  
+	System.out.println(s1.rollno+" "+s1.name);  
+	System.out.println(s2.rollno+" "+s2.name);  
+	  
+	}catch(CloneNotSupportedException c){}  
+	  
+	}  
+	}  
